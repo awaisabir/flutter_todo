8254275f203eb3f9e1d512cf9b3cc8ae3dc53931
awaisabir <aqureshi@klipfolio.com>
2019-02-02 17:09:59 -0500

    Playing around with dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(
+        title: 'This is my title being passed down',
+        subTitle: 'This is the subtitle'
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({this.title, this.subTitle}) : super();
 
   final String title;
+  final String subTitle;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -39,16 +43,22 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'The button has been pushed ',
+            Container(child: 
+                Text(
+                '${widget.subTitle}',
+                style: TextStyle(
+                  fontSize: 25.0,
+                )
+              ),
+              margin: EdgeInsets.only(bottom: 25),
             ),
             Text(
-              '$_counter time(s)!',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+              'The button has been pushed $_counter time(s)!',
+            )
+          ]
+        )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
